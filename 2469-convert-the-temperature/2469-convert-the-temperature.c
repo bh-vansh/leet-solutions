c15ba783612b558c60027b4f01291e5afce27196
Bharat vansh Singh <rsvansh.bharat216@gmail.com>
2026-09-20 19:56:28 +0530

    Time: 0 ms (100%), Space: 8.8 MB (33.08%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.c
+++ 2469-convert-the-temperature/2469-convert-the-temperature.c
@@ -2,12 +2,9 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 double* convertTemperature(double celsius, int* returnSize) {
-    double k,f,i;
-        k=celsius+273.15;
-        f=celsius*1.80+32.00;
     double *temp=malloc(2*sizeof(double));
-        temp[0]=k;
-        temp[1]=f;
+        temp[0]=celsius+273.15;
+        temp[1]=celsius*1.80+32.00;
     *returnSize=2;
     return temp;
 }

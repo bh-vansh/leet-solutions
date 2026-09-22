d063fdda1f5fbb1df3973dc3fef1050a71e9ca3f
bh-vansh <rsvansh.bharat216@gmail.com>
2026-09-23 00:09:21 +0530

    Add Maximum 69 Number solution

---
+++ 1323-maximum-69-number/solution.c
@@ -0,0 +1,27 @@
+int maximum69Number (int num) {
+    int i,a,org=num;
+    int post=-1,position=0;
+
+    while(org!=0)
+    {
+        a=org%10;
+        if(a==6)
+        {
+            post=position;
+        }
+        position++;
+        org=org/10;
+    }
+
+    if(post!=-1)
+    {
+        int mult=1;
+        for(i=0;i<post;i++)
+        {
+            mult=mult*10;
+        }
+        num=num+3*mult;
+    }
+
+    return num;
+}

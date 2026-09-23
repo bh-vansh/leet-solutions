52a8c3d682f48fc47fefc72df0b0483f635b1c5a
Bharat vansh Singh <rsvansh.bharat216@gmail.com>
2026-09-23 23:16:40 +0530

    Time: 227 ms (46.94%), Space: 8.7 MB (2.04%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.c
@@ -0,0 +1,18 @@
+int countTriples(int n) {
+    
+    int a,b,c,count=0;
+    for(a=1;a<=n;a++)
+    {
+        for(b=1;b<=n;b++)
+        {
+            for(c=1;c<=n;c++)
+            {
+                if(a*a+b*b==c*c)
+                {
+                    count++;
+                }
+            }
+        }    
+    }
+    return count;
+}

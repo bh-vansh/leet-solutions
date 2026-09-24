3e852341055a1d9cf7f63324872329fc877a7226
Bharat vansh Singh <rsvansh.bharat216@gmail.com>
2026-09-24 23:29:21 +0530

    Time: 0 ms (100%), Space: 10.4 MB (63.14%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.c
@@ -0,0 +1,17 @@
+int minTimeToVisitAllPoints(int** points, int pointsSize, int* pointsColSize) {
+    int time = 0;
+
+    for (int i = 1; i < pointsSize; i++) {
+        int dx = points[i][0] - points[i - 1][0];
+        int dy = points[i][1] - points[i - 1][1];
+
+        if (dx < 0)
+            dx = -dx;
+
+        if (dy < 0)
+            dy = -dy;
+
+        time += (dx > dy) ? dx : dy;
+    }
+    return time;
+}
